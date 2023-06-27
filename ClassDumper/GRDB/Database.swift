acc5e83e230d1acc88ca156145166cc414824678
Drew Volz <volz.drew@gmail.com>
2023-06-26 23:03:45 -0700

    remove the randomized static names elements

--- ClassDumper/GRDB/Database.swift
+++ ClassDumper/GRDB/Database.swift
@@ -2,23 +2,13 @@ import Files
 
 // Convenience `File` methods for the app.
 extension File {
-    private static let names = [
-        "Arthur", "Anita", "Barbara", "Bernard", "Craig", "Chiara", "David",
-        "Dean", "Éric", "Elena", "Fatima", "Frederik", "Gilbert", "Georgette",
-        "Henriette", "Hassan", "Ignacio", "Irene", "Julie", "Jack", "Karl",
-        "Kristel", "Louis", "Liz", "Masashi", "Mary", "Noam", "Nicole",
-        "Ophelie", "Oleg", "Pascal", "Patricia", "Quentin", "Quinn", "Raoul",
-        "Rachel", "Stephan", "Susie", "Tristan", "Tatiana", "Ursule", "Urbain",
-        "Victor", "Violette", "Wilfried", "Wilhelmina", "Yvon", "Yann",
-        "Zazie", "Zoé"]
-    
     /// Creates a new file with random name and random score
     static func makeRandom(id: Int64? = nil) -> File {
         File(
             id: id,
-            name: names.randomElement()!,
-            folder: names.randomElement()!,
-            contents: names.randomElement()!
+            name: "B",
+            folder: "A",
+            contents: "C"
         )
     }
     
