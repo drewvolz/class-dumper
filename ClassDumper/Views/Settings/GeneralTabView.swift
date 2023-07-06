038219db3f74356e47987e7296d2b010e7a5d864
Drew Volz <volz.drew@gmail.com>
2023-07-06 16:26:59 -0700

    move individual views into their own viewbuilders

--- ClassDumper/Views/Settings/GeneralTabView.swift
+++ ClassDumper/Views/Settings/GeneralTabView.swift
@@ -7,32 +7,48 @@ struct GeneralSettingsView: View {
 
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
-                Picker("", selection: $theme) {
-                    ForEach(CodeEditor.availableThemes) { theme in
-                        Text("\(theme.rawValue.capitalized)")
-                            .tag(theme)
-                    }
-                }
-                .labelsHidden()
-                .pickerStyle(.menu)
-                .fixedSize()
-                .modifier(PreferencesTabViewModifier(sectionTitle: "Code theme"))
-
-                Stepper(value: $fontSize, in: 1...1000) {
-                    TextField("", text: $fontSize.toTextFieldLabel)
-                        .labelsHidden()
-                        .textFieldStyle(.roundedBorder)
-                        .multilineTextAlignment(.trailing)
-                        .frame(minWidth: 55, maxWidth: 85)
-                        .fixedSize()
-                }
-                .modifier(PreferencesTabViewModifier(sectionTitle: "Font size"))
-
-                DeleteFilesButton("Delete all saved data", afterWithPrompt: {
-                    // noop, deletion with prompting is handled in the button
-                    // but side effects may be placed here if desired.
-                })
-                .modifier(PreferencesTabViewModifier(sectionTitle: "Database"))
+                ThemePicker()
+                FontSizePicker()
+                ResetDataButton()
         }
+    }
+}
+
+extension GeneralSettingsView {
+    
+    @ViewBuilder
+    func ThemePicker() -> some View {
+        Picker("", selection: $theme) {
+            ForEach(CodeEditor.availableThemes) { theme in
+                Text("\(theme.rawValue.capitalized)")
+                    .tag(theme)
+            }
+        }
+        .labelsHidden()
+        .pickerStyle(.menu)
+        .fixedSize()
+        .modifier(PreferencesTabViewModifier(sectionTitle: "Code theme"))
+    }
+    
+    @ViewBuilder
+    func FontSizePicker() -> some View {
+        Stepper(value: $fontSize, in: 1...1000) {
+            TextField("", text: $fontSize.toTextFieldLabel)
+                .labelsHidden()
+                .textFieldStyle(.roundedBorder)
+                .multilineTextAlignment(.trailing)
+                .frame(minWidth: 55, maxWidth: 85)
+                .fixedSize()
+        }
+        .modifier(PreferencesTabViewModifier(sectionTitle: "Font size"))
+    }
+    
+    @ViewBuilder
+    func ResetDataButton() -> some View {
+        DeleteFilesButton("Delete all saved data", afterWithPrompt: {
+            // noop, deletion with prompting is handled in the button
+            // but side effects may be placed here if desired.
+        })
+        .modifier(PreferencesTabViewModifier(sectionTitle: "Database"))
     }
 }
