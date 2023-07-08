60fdf0393cb1ddbd00069538c7c751c7b7378145
Drew Volz <volz.drew@gmail.com>
2023-07-07 23:10:09 -0700

    add open call for a default mac app and check for a guaranteed result

--- ClassDumperUITests/ClassDumpUserUITests.swift
+++ ClassDumperUITests/ClassDumpUserUITests.swift
@@ -5,15 +5,23 @@ protocol Screen {
 }
 
 final class ClassDumperUITests: UITestCase {
-    // TODO: CI: copy the embedded class-dump executable as the test file
-    let testDump = "class-dump"
-
+    /// This test passes the `--uitesting` flag  to the file picker which resolves the final
+    /// hardcoded url path to the bundled `class-dump` executable as our test data.
+    ///
+    /// We still want to pass in a file that XCUITest can locate in the view hierarchy but
+    /// so `openApp` is handed a name we expect to exist, and `checkFirstResult`
+    /// is handed a different name we expect to exist. This allows us to test the app's ability
+    /// to import and parse from an end-to-end perspective.
+    ///
+    /// In this case Automator is guaranteed to be found on standard macOS, and because
+    /// we switch out the imported path upon successful selection and closing of the window,
+    /// we are safe to assume that class-dump will be our chosen file at the end of this flow.
     func testSidebar() {
         Sidebar(app: app)
             .resetState()
             .checkListDoesNotExist()
-            .openApp(named: testDump)
+            .openApp(named: "Automator")
             .checkListExists()
-            .checkFirstResult(contains: testDump)
+            .checkFirstResult(contains: "class-dump")
     }
 }
